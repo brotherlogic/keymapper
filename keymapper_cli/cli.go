9aea887a79583babaf9df77bd7470ab2b25dd68d
Simon Tucker <brotherlogic@gmail.com>
2020-10-24 09:49:12 -0700

    Register the server

---
+++ keymapper_cli/cli.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/brotherlogic/goserver/utils"
+
+	pb "github.com/brotherlogic/keymapper/proto"
+
+	//Needed to pull in gzip encoding init
+	_ "google.golang.org/grpc/encoding/gzip"
+)
+
+func main() {
+	ctx, cancel := utils.BuildContext("keymapper-cli", "keymapper")
+	defer cancel()
+
+	conn, err := utils.LFDialServer(ctx, "keymapper")
+	if err != nil {
+		log.Fatalf("Unable to dial: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewKeymapperServiceClient(conn)
+
+	switch os.Args[1] {
+	case "set":
+		_, err := client.Set(ctx, &pb.SetRequest{Key: os.Args[2], Value: os.Args[3]})
+		if err != nil {
+			log.Fatalf("Error on Add Record: %v", err)
+		}
+	case "get":
+		res, err := client.Get(ctx, &pb.GetRequest{Key: os.Args[2]})
+		if err != nil {
+			log.Fatalf("Error in listing: %v", err)
+		}
+
+		fmt.Printf("%v => %v\n", res.GetKey().GetKey(), res.GetKey().GetValue())
+
+	}
+
+}
